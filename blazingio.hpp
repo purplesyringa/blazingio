50d9a538ddb5964c5dc9b1128ab9c555f674cabc
Alisa Sireneva <me@purplesyringa.moe>
2024-01-31 14:03:49 +0300

    Strip out a bit more with floats

--- blazingio.hpp
+++ blazingio.hpp
@@ -191,12 +191,14 @@ struct blazingio_istream {
 		}
 	}
 
+#	ifdef FLOAT
 	template<typename T>
 	static constexpr T exps[] = {
 		1e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1,
 		1,
 		1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13, 1e14, 1e15
 	};
+#	endif
 
 	template<typename T>
 	T read_arithmetic() {

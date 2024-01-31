441fd65646af66453b7710dcc66e3ec4b4bcb89e
Alisa Sireneva <me@purplesyringa.moe>
2024-02-01 02:20:00 +0300

    Code minimisation

--- blazingio.hpp
+++ blazingio.hpp
@@ -486,11 +486,10 @@ struct blazingio_ostream {
 			constexpr auto computed = [] {
 				int low_digits = 1;
 				T coeff = 10;
-				while (low_digits * 2 < MaxDigits) {
-					low_digits *= 2;
+				while (low_digits *= 2 < MaxDigits) {
 					coeff *= coeff;
 				}
-				return pair{low_digits, coeff};
+				return pair{low_digits / 2, coeff};
 			}();
 			constexpr int low_digits = computed.first;
 			constexpr T coeff = computed.second;

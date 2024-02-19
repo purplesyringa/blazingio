b1da584166de4d20d8017bb48ab7a5386ca5b2a1
Alisa Sireneva <me@purplesyringa.moe>
2024-02-20 01:54:55 +0300

    Run tests on MINGW64 system

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 
 
-tmp = "/tmp"
+tmp = os.environ.get("TEMP", "/tmp")
 
 if len(sys.argv) >= 2 and sys.argv[1] == "--cross":
     arch = sys.argv[2]
@@ -19,17 +19,17 @@ elif len(sys.argv) >= 2 and sys.argv[1] == "--cross-windows":
         subprocess.run([f"{arch}-w64-mingw32-g++", "-static", source, "-o", target, "-iquote", ".", f"-DBLAZINGIO=\"{blazingio}\""], check=True)
 elif len(sys.argv) >= 2 and sys.argv[1] == "--msvc":
     arch = platform.machine()
-    if arch == "AMD64":
-        arch = "x86_64"
     def compile(source, target, blazingio):
         subprocess.run([f"cl", source, "/I.", f"/DBLAZINGIO=\"{blazingio}\"", f"/Fe{target}", "/std:c++17", "/EHsc", "/nologo"], check=True)
-    tmp = os.environ["TEMP"]
     os.environ["CPP"] = "C:\\msys64\\usr\\bin\\bash.exe -l -c \"exec cpp $*\" cpp"
     os.environ["MSYSTEM"] = "UCRT64"
 else:
     arch = platform.machine()
     def compile(source, target, blazingio):
         subprocess.run(["g++", source, "-o", target, "-iquote", ".", f"-DBLAZINGIO=\"{blazingio}\""], check=True)
+
+if arch == "AMD64":
+    arch = "x86_64"
 
 
 def iterate_config(config, props = []):

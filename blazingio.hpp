a00ba777674a2dc3ce43bb6741d2aa1ef5103242
Alisa Sireneva <me@purplesyringa.moe>
2024-02-19 15:14:55 +0300

    Fix memory map size

--- blazingio.hpp
+++ blazingio.hpp
@@ -156,7 +156,7 @@ struct istream_impl {
         ensure(base)
         ensure(VirtualFree(base, 0, MEM_RELEASE))
         // Map the file there
-        size_t mmaped_region_size = (file_size + 8191) & -65536;
+        size_t mmaped_region_size = (file_size + 4095) & -65536;
         // If we remove this if and always call CreateFileMapping, it's going to interpret 0 as
         // "max", which we don't want.
         if (mmaped_region_size)

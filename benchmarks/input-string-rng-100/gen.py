efba3d722c5c4722654cd56d0bd8e9fbebcf3ade
Alisa Sireneva <me@purplesyringa.moe>
2024-02-25 16:50:36 +0300

    Reduce memory pressure from benchmarks

--- benchmarks/input-string-rng-100/gen.py
+++ benchmarks/input-string-rng-100/gen.py
@@ -3,5 +3,5 @@ import sys
 
 sys.stdout.buffer.write(b" ".join(
     bytes(random.randint(33, 255) for _ in range(random.randint(1, 100)))
-    for _ in range(10000000)
+    for _ in range(5000000)
 ))
--- benchmarks/output-string-rng-100/source_blazingio.cpp
+++ benchmarks/output-string-rng-100/source_blazingio.cpp
@@ -10,7 +10,7 @@ int main() {
         s[j] = gen() % (255 - 33 + 1) + 33;
     }
     uint64_t n = 1;
-    for (size_t i = 0; i < 10000000; i++) {
+    for (size_t i = 0; i < 5000000; i++) {
         size_t length = (n >> 32) % 99 + 1;
         std::cout << std::string_view(s, length) << ' ';
         n *= 0x7432974328912321;

da3db0f53809c78a5af9139e7f19782fc5abc796
Alisa Sireneva <me@purplesyringa.moe>
2024-02-25 18:13:55 +0300

    Preemptively fix warning

--- benchmarks/output-exponentially-random-float/source_cstdio_g.cpp
+++ benchmarks/output-exponentially-random-float/source_cstdio_g.cpp
@@ -7,7 +7,7 @@ int main() {
     std::minstd_rand gen;
     for (size_t i = 0; i < 500000; i++) {
         int32_t rng = gen();
-        float x = exp(rng / 46684427.);
+        float x = (float)exp(rng / 46684427.);
         if (rng & 1) {
             x = -x;
         }

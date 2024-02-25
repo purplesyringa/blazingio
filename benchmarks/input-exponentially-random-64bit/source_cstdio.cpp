133b87c6e1a389b7afcc64b3806a9c21654ef166
Alisa Sireneva <me@purplesyringa.moe>
2024-02-25 16:35:24 +0300

    Fix typo

--- benchmarks/input-exponentially-random-64bit/source_cstdio.cpp
+++ benchmarks/input-exponentially-random-64bit/source_cstdio.cpp
@@ -5,7 +5,7 @@
 int main() {
     int64_t n;
     uint64_t sum = 0;
-    while (scanf("%ld", &n) > 0) {
+    while (scanf("%" PRId64, &n) > 0) {
         sum += n;
     }
     // Do not optimize out reading of n

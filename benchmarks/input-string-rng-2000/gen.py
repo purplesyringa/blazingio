f71ffbdfba8a861e6126bd99b1841ef6a6a2cfee
Alisa Sireneva <me@purplesyringa.moe>
2024-02-26 02:16:01 +0300

    Update benchmarks

--- benchmarks/input-string-rng-2000/gen.py
+++ benchmarks/input-string-rng-2000/gen.py
@@ -5,5 +5,5 @@ import sys
 table = bytes(random.randint(33, 255) for _ in range(256))
 sys.stdout.buffer.write(b" ".join(
     random.randbytes(random.randint(1, 2000)).translate(table)
-    for _ in range(2000000)
+    for _ in range(1000000)
 ))
--- benchmarks/output-string-rng-2000/source_blazingio.cpp
+++ benchmarks/output-string-rng-2000/source_blazingio.cpp
@@ -10,8 +10,8 @@ int main() {
         s[j] = gen() % (255 - 33 + 1) + 33;
     }
     uint64_t n = 1;
-    for (size_t i = 0; i < 10000000; i++) {
-        size_t length = (n >> 32) % 99 + 1;
+    for (size_t i = 0; i < 1000000; i++) {
+        size_t length = (n >> 32) % 2000 + 1;
         std::cout << std::string_view(s, length) << ' ';
         n *= 0x7432974328912321;
     }

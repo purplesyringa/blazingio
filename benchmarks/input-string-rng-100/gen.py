e450036bb55698797c82e67f00e8af72a0d8f831
Alisa Sireneva <me@purplesyringa.moe>
2024-02-26 03:59:57 +0300

    Halve a size

--- benchmarks/input-string-rng-100/gen.py
+++ benchmarks/input-string-rng-100/gen.py
@@ -4,5 +4,5 @@ import sys
 table = bytes(random.randint(33, 255) for _ in range(256))
 sys.stdout.buffer.write(b" ".join(
     random.randbytes(random.randint(1, 100)).translate(table)
-    for _ in range(10000000)
+    for _ in range(5000000)
 ))
